00974e4ac4ce06bbd1771ba0f2ea7cc1cd68bf8d
João Cavalcante <joaocavalcante25@gmail.com>
2023-12-05 05:14:38 -0400

    feat: melhorando home

--- src/components/Portifolio.jsx
+++ src/components/Portifolio.jsx
@@ -2,22 +2,16 @@
 import React from 'react';
 
 const Portfolio = () => {
-  return (
-    <>
-        <div className="container h-full w-full">
-          
-            
-            <section className="mb-8 bg-indigo-950 text-cyan-50 p-10">
-                <h2 className="text-3xl font-bold mb-4">Resumo Profissional</h2>
-                <p>Sou um Desenvolvedor Full Stack apaixonado por transformar ideias em soluções inovadoras...</p>
-            </section>
+  return <>
+        <section className="mb-8 bg-indigo-950 text-cyan-50 p-10">
+            <h2 className="text-3xl font-bold mb-4">Resumo Profissional</h2>
+            <p>Sou um Desenvolvedor Full Stack apaixonado por transformar ideias em soluções inovadoras...</p>
+        </section>
 
-            <section className="mb-8">
-                
-            </section>
-      </div>
-      </>
-  );
+        <section className="mb-8">
+            
+        </section>
+    </>
 };
 
 export default Portfolio;
